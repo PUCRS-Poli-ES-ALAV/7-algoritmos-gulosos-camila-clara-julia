b2557ee597db83e40c250abd0cb053cf7d09bf9d
juAlberti <julia.maia@acad.pucrs.br>
2022-04-04 20:39:13 -0300

    1 ainda nao pronto

---
+++ Exercicio1.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+  
+class Exercicio1
+{
+    static int centavos[] = {1, 5, 10, 25, 100};
+    static int n = centavos.length;
+  
+    static void findMin(int V)
+    {
+        ArrayList<String> ans = new ArrayList<String>();
+  
+        for (int i = n - 1; i >= 0; i--)
+        {
+            while (V >= centavos[i]) 
+            {
+                V -= centavos[i];
+                
+                ans.add(centavos[i]);
+            }
+        }
+  
+        for (int i = 0; i < ans.size(); i++)
+        {
+            System.out.print(
+                " " + ans.elementAt(i));
+        }
+    }
+   
+    public static void main(String[] args) 
+    {
+        int n = 93;
+        System.out.print(
+            "Following is minimal number "
+            +"of change for " + n + ": ");
+        findMin(n);
+    }
+}
