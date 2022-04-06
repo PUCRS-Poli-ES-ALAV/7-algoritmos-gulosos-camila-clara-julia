a84ccb5ccf004addcd9b678fb36c57119633549e
Camila Vicari Maccari <Camila.Maccari@edu.pucrs.br>
2022-04-06 20:20:38 -0300

    Arrumando o exercicio1

--- Exercicio1.java
+++ Exercicio1.java
@@ -7,7 +7,7 @@ class Exercicio1
   
     static void findMin(int V)
     {
-        ArrayList<String> ans = new ArrayList<String>();
+        ArrayList<Integer> ans = new ArrayList<Integer>();
   
         for (int i = n - 1; i >= 0; i--)
         {
@@ -21,8 +21,7 @@ class Exercicio1
   
         for (int i = 0; i < ans.size(); i++)
         {
-            System.out.print(
-                " " + ans.elementAt(i));
+            System.out.print(" " + ans.get(i));
         }
     }
    
